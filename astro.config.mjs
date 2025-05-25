acbcd2d0ec9635284745083e4539a1fa2ce519a7
f-vojnovski <48998036+f-vojnovski@users.noreply.github.com>
2025-05-26 00:23:12 +0200

    add action for hosting on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+  site: 'https://f-vojnovski.github.io',
 });
